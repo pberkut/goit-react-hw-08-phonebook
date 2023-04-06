a6053c86c2b5e79ed3f9514a9985fa43300ab2d7
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-04-06 17:45:15 +0300

    Code refactoring addMatcher

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -53,37 +53,37 @@ const ContactItem = ({ index, contact }) => {
       <span>{index + 1}.</span>
       <ContactBlock>
         {isEdit ? (
-          <Label>
-            Name
-            <Input
-              name="name"
-              value={name}
-              onChange={handleChange}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              type="text"
-            />
-          </Label>
+          <>
+            <Label>
+              Name
+              <Input
+                name="name"
+                value={name}
+                onChange={handleChange}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                type="text"
+              />
+            </Label>
+            <Label>
+              Phone
+              <Input
+                name="phone"
+                value={phone}
+                onChange={handleChange}
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                type="tel"
+              />
+            </Label>
+          </>
         ) : (
-          <span>{name}:</span>
-        )}
-
-        {isEdit ? (
-          <Label>
-            Phone
-            <Input
-              name="phone"
-              value={phone}
-              onChange={handleChange}
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              type="tel"
-            />
-          </Label>
-        ) : (
-          <span>{phone}</span>
+          <>
+            <span>{name}:</span>
+            <span>{phone}</span>
+          </>
         )}
       </ContactBlock>
       <ButtonBlock>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import {
   fetchContacts,
   addContact,
@@ -29,38 +29,51 @@ const contactsSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        handleFulfilled(state);
         state.items = action.payload;
       })
-      .addCase(fetchContacts.rejected, handleRejected)
-      .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
         state.items.push(action.payload);
       })
-      .addCase(addContact.rejected, handleRejected)
-      .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
-
         const index = state.items.findIndex(
           contact => contact.id === action.payload
         );
         state.items.splice(index, 1);
       })
-      .addCase(deleteContact.rejected, handleRejected)
-      .addCase(editContact.pending, handlePending)
       .addCase(editContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
-
         const index = state.items.findIndex(
           contact => contact.id === action.payload.id
         );
         state.items.splice(index, 1, action.payload);
       })
-      .addCase(editContact.rejected, handleRejected);
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.pending,
+          addContact.pending,
+          deleteContact.pending,
+          editContact.pending
+        ),
+        handlePending
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.fulfilled,
+          addContact.fulfilled,
+          deleteContact.fulfilled,
+          editContact.fulfilled
+        ),
+        handleFulfilled
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.rejected,
+          addContact.rejected,
+          deleteContact.rejected,
+          editContact.rejected
+        ),
+        handleRejected
+      );
   },
 });
 
